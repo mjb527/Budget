b011f3a8bb081bd8a73ccfd3fd6cb991cb1babbf
Matt <mbatko0914@gmail.com>
2020-07-06 18:44:05 -0400

    missing route fixed

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
 
+router.get('/', (req, res) => {
+  res.sendFile('index.html');
+});
+
 router.post("/api/transaction", ({body}, res) => {
   Transaction.create(body)
     .then(dbTransaction => {
