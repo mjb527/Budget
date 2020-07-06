d0e872967d2e7442d30a1d6a04c8fcf67fd52896
Matt <mbatko0914@gmail.com>
2020-07-06 18:45:31 -0400

    revert back from last changes

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,6 @@
 const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
 
-router.get('/', (req, res) => {
-  res.sendFile('index.html');
-});
-
 router.post("/api/transaction", ({body}, res) => {
   Transaction.create(body)
     .then(dbTransaction => {
